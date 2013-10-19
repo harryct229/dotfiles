ced67a685809e780a558f222015d298aa91bbdab
Thanh Nguyen <congthanh991@gmail.com>
2013-10-19 22:02:58 +0700

    -fix awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -86,3 +86,80 @@ require("settings.startup")
 -- wallpaper snippet
 local mywp = require("settings.wallpaper")
 
+-- Functions to help launch run commands in a terminal using ":" keyword
+function check_for_terminal (command)
+  if command:sub(1,1) == ":" then
+    command = terminal .. ' -e "' .. command:sub(2) .. '"'
+  end
+  awful.util.spawn(command)
+end
+
+function clean_for_completion (command, cur_pos, ncomp, shell)
+  local term = false
+  if command:sub(1,1) == ":" then
+    term = true
+    command = command:sub(2)
+    cur_pos = cur_pos - 1
+  end
+  command, cur_pos =  awful.completion.shell(command, cur_pos,ncomp,shell)
+  if term == true then
+    command = ':' .. command
+    cur_pos = cur_pos + 1
+  end
+  return command, cur_pos
+end
+
+-- Eval with result
+function usefuleval(s)
+  local f, err = loadstring("return "..s)
+  if not f then
+    f, err = loadstring(s)
+  end
+
+  if f then
+    setfenv(f, _G)
+    local ret = { pcall(f) }
+    if ret[1] then
+      -- Ok
+      table.remove(ret, 1)
+      local highest_index = #ret
+      for k, v in pairs(ret) do
+        if type(k) == "number" and k > highest_index then
+          highest_index = k
+        end
+        ret[k] = select(2, pcall(tostring, ret[k])) or "<no value>"
+      end
+      -- Fill in the gaps
+      for i = 1, highest_index do
+        if not ret[i] then
+          ret[i] = "nil"
+        end
+      end
+
+      if highest_index > 0 then
+        local ret_txt = tostring(table.concat(ret, ", "))
+        naughty.notify({
+          title = "Result:",
+          text = ret_txt,
+          screen = mouse.screen
+        })
+      else
+        naughty.notify({
+          title = "Result:",
+          text = "Nothing",
+          screen = mouse.screen
+        })
+      end
+    else
+      err = ret[2]
+    end
+    if err then
+      local err_txt = err
+      naughty.notify({
+        title = "Error:",
+        text = err_txt,
+        screen = mouse.screen
+      })
+    end
+  end
+end
